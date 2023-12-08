0bc1def163f6c2392847f69c4c61afc5839a0151
Matheus Silva de Sousa <matheussilvades@gmail.com>
2023-12-08 08:09:38 -0300

    .

--- dart/1178.dart
+++ dart/1178.dart
@@ -1,32 +1,21 @@
 import 'dart:io';
 
-String dec(sub) {
-  double x = int.parse(sub) * 9999.99999;
-  double c = double.parse('0.' + x.toStringAsFixed(0));
-  if (sub.startsWith(RegExp(r"^0[1-9]+"))) {
-    c = double.parse('0.0' + x.toStringAsFixed(0));
-  } else if (sub.startsWith(RegExp(r"^00[1-9]+"))) {
-    c = double.parse('0.00' + x.toStringAsFixed(0));
-  } else if (sub.startsWith(RegExp(r"^000[1-9]+"))) {
-    c = double.parse('0.000' + x.toStringAsFixed(0));
-  } else if (sub.startsWith(RegExp(r"^0000[1-9]+"))) {
-    c = double.parse('0.0000' + x.toStringAsFixed(0));
-  } else if (sub.startsWith(RegExp(r"^00000[1-9]+"))) {
-    c = double.parse('0.00000' + x.toStringAsFixed(0));
+String fix(String str) {
+  List<String> spl = str.split('.');
+  
+  if (int.parse(spl[1][4]) <= 5 && int.parse(spl[1][5]) == 0) {
+    return spl[0] +'.'+ spl[1].substring(0, 4);
   }
-  return c.toStringAsFixed(4).split('.')[1];
+
+  return double.parse(spl[0] +'.'+ spl[1]).toStringAsFixed(4);
 }
 
 void main() {
   double x = double.parse(stdin.readLineSync()!);
 
   for (int i = 0; i < 100; i++) {
-    List<String> s = x.toString().split('.');
-    if (s[1].contains('e')) {
-      print('N[$i] = 0.0000');
-    } else {
-      print("N[$i] = ${s[0]}.${dec(s[1])}");
-    }
+    String s = x.toStringAsFixed(6);
+    print("N[$i] = ${fix(s)}");
     x /= 2;
   }
 }

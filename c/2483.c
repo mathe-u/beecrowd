f2c86ea2a042c5909e80c8d82b5fcfbd5d157487
Matheus Silva <sousamatheus076@gmail.com>
2023-12-24 17:41:37 -0300

    2483

---
+++ c/2483.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  scanf("%d", &i);
+
+  printf("Feliz nat");
+
+  for (int x = 0; x < i; x++) {
+    printf("a");
+  }
+  
+  printf("l!\n");
+}
---
+++ dart/2483.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  int i = int.parse(stdin.readLineSync()!);
+
+  print("Feliz nat" + "a" * i + "l!");
+}

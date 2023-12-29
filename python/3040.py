38fec58ff6c518fcfaa1308e835168ceba2bbc83
Matheus Silva <sousamatheus076@gmail.com>
2023-12-29 12:41:32 -0300

    3040

---
+++ python/3040.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for _ in range(n):
+    h, d, g = map(int, input().split())
+
+    if h >= 200 and h <= 300 and d >= 50 and g >= 150:
+        print('Sim')
+    else:
+        print('Nao')

d4b72b10be84e477e89236007dab995a6091494a
Matheus Silva <matheussilvades@gmail.com>
2024-01-12 19:51:56 -0300

    3093

---
+++ python/3093.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for _ in range(n):
+    monte = input()
+
+    if 'A' in monte and 'J' in monte and 'Q' in monte and 'K' in monte:
+        print('Aaah muleke')
+    else:
+        print('Ooo raca viu')
